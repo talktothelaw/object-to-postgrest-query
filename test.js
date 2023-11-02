0f98d71d54a846d393a388150ad1e435fc5d5de4
lawrence nwoko <nwokolawrence6@gmail.com>
2023-11-02 11:37:24 +0100

    Added 'eq' condition to postgrestQuery in test.js

--- test.js
+++ test.js
@@ -1,18 +1,12 @@
 const objectToPostgrestQuery = require('./lib/cjs/index').default
 
-const obj = {
-	limit: 1,
-	offset: 1,
-	order: {
-		updated_at: "asc.nullsfirst"
-	},
-	slug:  {ilike:`*${"undefined"}*`}
-};
-
 
 const postgrestQuery = objectToPostgrestQuery({
 	limit: Number(2),
 	offset: Number(100),
+	s:{
+		eq: ""
+	},
 	order: {
 		slug: false,
 		updated_at:  "desc",
